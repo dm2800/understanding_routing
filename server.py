9d18b8ee4d4a57f369efcc3794ccd4fa1b5b19d4
dm2800 <danielmurcia@me.com>
2022-02-28 23:24:01 -0800

    initial commit

---
+++ server.py
@@ -0,0 +1,33 @@
+from this import d
+from flask import Flask 
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>')
+def hi(name):
+    return f'Hi {name.capitalize()}!'
+
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat(word, num):
+    return f'{word * num}'
+
+
+# @app.route('/')
+# def error():
+#     return 'Sorry! No response. Try Again.'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
